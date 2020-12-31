2855b5f07f78f15b2a29fb5979cf09ecffbddb57
KenkaRaidon <61855947+KenkaRaidon@users.noreply.github.com>
2020-12-31 02:19:52 -0800

    Database Actualizada

--- database/peluqueria.sql
+++ database/peluqueria.sql
@@ -29,17 +29,17 @@ CREATE TABLE `cita` (
   `servicio` varchar(50) NOT NULL,
   `fecha_cita` date NOT NULL,
   `hora_cita` time NOT NULL,
-  `status` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT 'EN PROCESO',
   PRIMARY KEY (`id_cita`),
-  KEY `id_cliente` (`id_cliente`),
-  KEY `id_mascota` (`id_mascota`),
   KEY `servicio` (`servicio`),
+  KEY `cita_ibfk_1` (`id_cliente`),
+  KEY `cita_ibfk_2` (`id_mascota`),
   KEY `status` (`status`),
-  CONSTRAINT `cita_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente_mascotas` (`id_cliente`),
-  CONSTRAINT `cita_ibfk_2` FOREIGN KEY (`id_mascota`) REFERENCES `cliente_mascotas` (`id_mascota`),
+  CONSTRAINT `cita_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente_mascotas` (`id_cliente`) ON DELETE CASCADE,
+  CONSTRAINT `cita_ibfk_2` FOREIGN KEY (`id_mascota`) REFERENCES `cliente_mascotas` (`id_mascota`) ON DELETE CASCADE,
   CONSTRAINT `servicio` FOREIGN KEY (`servicio`) REFERENCES `servicio_cita` (`servicio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `status` FOREIGN KEY (`status`) REFERENCES `status_cita` (`status`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +48,7 @@ CREATE TABLE `cita` (
 
 LOCK TABLES `cita` WRITE;
 /*!40000 ALTER TABLE `cita` DISABLE KEYS */;
-INSERT INTO `cita` VALUES (1,2,5,'BAÑO','2020-12-27','22:31:00','EN PROCESO'),(2,7,11,'DRENADO DE GLANDULAS ANALES','2020-12-28','22:32:00','EN PROCESO');
+INSERT INTO `cita` VALUES (1,2,5,'BAÑO','2020-12-27','22:31:00','FINALIZADO'),(3,31,29,'CORTE DE UÑAS','2020-03-13','14:37:00','EN PROCESO'),(7,26,12,'CEPILLADO DE PELO','2020-12-30','13:39:00','EN PROCESO'),(8,2,19,'CORTE DE UÑAS','2020-12-29','14:05:00','EN PROCESO'),(9,26,12,'CORTE DE UÑAS','2021-02-10','04:05:00','EN PROCESO'),(12,2,19,'CEPILLADO DE PELO','2020-12-28','16:52:00','FINALIZADO'),(13,9,14,'CORTE DE PELO','2020-12-29','16:53:00','EN PROCESO'),(16,2,18,'CORTE DE UÑAS','2020-12-29','00:39:00','EN PROCESO'),(18,33,25,'BAÑO','2020-12-31','15:54:00','EN PROCESO');
 /*!40000 ALTER TABLE `cita` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,7 +70,7 @@ CREATE TABLE `cliente` (
   `calle` varchar(45) DEFAULT NULL,
   `numero` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_cliente`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,7 +79,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (2,'Juan','Lopez','Maria','m@gmail.com','6161908988','ALTA MAR','',0),(4,'Juan','Posada','','','','ALTA MAR','',0),(6,'Izamar','Posada','','','','0','',0),(7,'German','Lopez','','','','0','',0),(9,'Lia','As','','','','0','',0),(14,'Izamar','Lopez','','','','0','',0),(24,'Maria','Rocio','','Rosy@hotmail.com','66615423','MODERNA','Catriles',12),(25,'Monica','Sanz','','','123098834','AZTECA','Kosco',13),(26,'Cleo','Soros','','soronita@lovecraft.com','6156278831','AZTECA','Jota',44),(27,'','','','','','','',0),(28,'','','','','','','',0);
+INSERT INTO `cliente` VALUES (2,'Juans','Lopez','Maria','m@gmail.com','6161908988','ALTA MAR','Lco',12),(4,'Juan','Posada','','','','ALTA MAR','',0),(6,'Izamar','Posada','','','','0','',0),(9,'Lia','As','','','','0','',0),(14,'Izamar','Lopez','','','','0','',0),(24,'Maria','Rocio','','Rosy@hotmail.com','66615423','MODERNA','Catriles',12),(26,'Cleo','Soros','','soronita@lovecraft.com','6156278831','AZTECA','Jota',44),(29,'Susana','Lopez','','','6161908988','ALTA MAR','Casa',12),(30,'Joana ','Dark','','rare@gmail.com','616123093','MODERNA','Plop',12),(31,'Nadia','Nascar','Noe','nn@hotmail.com','616778243','AZTECA','Catriles',12),(33,'Jugo','Susana','','jumex@lofi.com','616827312','BOSQUE DE OLIVOS','Koko',23);
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,7 +106,7 @@ CREATE TABLE `cliente_mascotas` (
 
 LOCK TABLES `cliente_mascotas` WRITE;
 /*!40000 ALTER TABLE `cliente_mascotas` DISABLE KEYS */;
-INSERT INTO `cliente_mascotas` VALUES (2,5),(2,18),(2,19),(2,24),(2,33),(2,35),(6,36),(7,11),(9,14),(25,7),(26,12);
+INSERT INTO `cliente_mascotas` VALUES (2,5),(2,18),(2,19),(2,24),(2,33),(2,35),(6,36),(9,14),(26,12),(31,29),(33,25);
 /*!40000 ALTER TABLE `cliente_mascotas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -149,7 +149,7 @@ CREATE TABLE `empleado` (
   `telefono` varchar(45) DEFAULT NULL,
   `fecha_empleo` date DEFAULT NULL,
   PRIMARY KEY (`id_empleado`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -158,7 +158,7 @@ CREATE TABLE `empleado` (
 
 LOCK TABLES `empleado` WRITE;
 /*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
-INSERT INTO `empleado` VALUES (2,'Joseju','Manuel','Hernandez','joseM@gmail.com','616726882','2000-11-12'),(3,'Hector','Mordor','Lopez','hectorMordor@gmail.com',NULL,NULL),(4,'Courtney Jane','Barnett',NULL,'cb@uabc.edu.mx','616567289',NULL),(5,'Mario','Lopez','Lopez','mariobros@gmail.com',NULL,NULL),(6,'Kurt','Cobain',NULL,NULL,'6661234567',NULL),(7,'Platon','Keifer',NULL,'plata@gmail.com',NULL,NULL),(8,'Charles','Weston',NULL,'charlesManson@gmail.com',NULL,NULL),(9,'Maria','Candelaria',NULL,'candy@gmail.com','612366728',NULL),(10,'Laura','Mendrano','Moriz',NULL,NULL,NULL),(11,'Lindsey','Jordan',NULL,'snailmail@gmail.com',NULL,NULL),(12,'Karolina','Herrera','Lopez','KC@gmail.com','','0000-00-00'),(13,'Kiko','Vega','','','','1999-02-15'),(14,'Paco','El Chato','','','','2000-11-12'),(15,'Frodo','Bolson','Cerrado','frodo@mordor.com','65690190','2001-11-15'),(16,'Luis','Mesa','','','','1999-02-12'),(17,'Pablo','Wear','','','','2220-11-12'),(20,'Maria','Colosa','Vega','Mary@gmail.com','6161762233','1999-02-11');
+INSERT INTO `empleado` VALUES (4,'Courtney Jane','Barnett',NULL,'cb@uabc.edu.mx','616567289',NULL),(5,'Mario','Lopez','Lopez','mariobros@gmail.com',NULL,NULL),(6,'Kurt','Cobain',NULL,NULL,'6661234567',NULL),(7,'Platon','Keifer',NULL,'plata@gmail.com',NULL,NULL),(8,'Charles','Weston',NULL,'charlesManson@gmail.com',NULL,NULL),(9,'Maria','Candelaria',NULL,'candy@gmail.com','612366728',NULL),(10,'Laura','Mendrano','Moriz',NULL,NULL,NULL),(11,'Lindsey','Jordan',NULL,'snailmail@gmail.com',NULL,NULL),(12,'Karolina','Herrera','Lopez','KC@gmail.com','','0000-00-00'),(13,'Kiko','Vega','','','','1999-02-15'),(14,'Paco','El Chato','','','','2000-11-12'),(15,'Frodo','Bolson','Cerrado','frodo@mordor.com','65690190','2001-11-15'),(16,'Luis','Mesa','','','','1999-02-12'),(17,'Pablo','Wear','','','','2220-11-12'),(20,'Maria','Colosa','Vega','Mary@gmail.com','6161762233','1999-02-11'),(21,'Jose','Juan','Lopez','h3h3@gmail.com','6167289933','2020-07-09'),(22,'Candy','Crush','Martinez','ccm@gmail.com','616123456','2020-10-03');
 /*!40000 ALTER TABLE `empleado` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,9 +177,9 @@ CREATE TABLE `historial` (
   `hora_finalizacion` time NOT NULL,
   `precio` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id_cita`),
-  KEY `id_empleado` (`id_empleado`),
-  CONSTRAINT `historial_ibfk_1` FOREIGN KEY (`id_cita`) REFERENCES `cita` (`id_cita`),
-  CONSTRAINT `historial_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`)
+  KEY `historial_ibfk_2` (`id_empleado`),
+  CONSTRAINT `historial_ibfk_1` FOREIGN KEY (`id_cita`) REFERENCES `cita` (`id_cita`) ON DELETE CASCADE,
+  CONSTRAINT `historial_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -189,6 +189,7 @@ CREATE TABLE `historial` (
 
 LOCK TABLES `historial` WRITE;
 /*!40000 ALTER TABLE `historial` DISABLE KEYS */;
+INSERT INTO `historial` VALUES (12,11,1,1,'18:56:00',34.34);
 /*!40000 ALTER TABLE `historial` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -378,11 +379,12 @@ CREATE TABLE `usuario` (
   `tipo_usuario` varchar(30) NOT NULL,
   `id_empleado` int(11) NOT NULL,
   PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `nombre_usuario` (`nombre_usuario`),
   KEY `tipo_usuario` (`tipo_usuario`),
-  KEY `id_empleado` (`id_empleado`),
+  KEY `usuario_ibfk_2` (`id_empleado`),
   CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`tipo_usuario`) REFERENCES `tipo_usuario` (`tipo_usuario`),
-  CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -391,6 +393,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'courtney1998','12345','ADMIN',4);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -403,4 +406,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-27 22:42:07
+-- Dump completed on 2020-12-31  2:18:36
